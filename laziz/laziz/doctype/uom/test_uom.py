6175dfdc3014d0ab3e79897a116a08126edc67e1
Beshoy <beshoyatef31@gmail.com>
2024-03-09 16:08:43 +0200

    inti master data

---
+++ laziz/laziz/doctype/uom/test_uom.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, beshoy atef and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testuom(FrappeTestCase):
+	pass
---
+++ laziz/laziz/doctype/uom/uom.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, beshoy atef and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('uom', {
+	// refresh: function(frm) {
+
+	// }
+});
