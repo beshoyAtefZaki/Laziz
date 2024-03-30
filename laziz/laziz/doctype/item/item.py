6d10dc3a9fe1ed21a9cfd5c10d237b46d4a8a990
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2024-03-30 13:43:45 +0200

    before pulling last changes

---
+++ laziz/laziz/doctype/item/item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, beshoy atef and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class Item(WebsiteGenerator):
+	pass
