0045ffcd1d38d31a15a1e2afd7ca1141011d4881
Beshoy <beshoyatef31@gmail.com>
2024-03-30 16:41:44 +0200

    day 4

--- laziz/laziz/doctype/item/item.py
+++ laziz/laziz/doctype/item/item.py
@@ -7,13 +7,52 @@ from frappe.model.document import Document
 from frappe.model.naming import getseries
 
 class Item(Document):
-	# def autoname(self):
-	# 	# select a project name based on customer
 
-	# 	#frappe.throw("Auto name method ")
-	# 	#prefix = f'P-{self.item_name}-'
-	# 	#frappe.throw(prefix)
-	# 	self.name =  f'P-{self.item_name}- 1' #getseries(prefix, 3)
 	def validate(self) :
 		pass
-		#frappe.throw("Validate Method")
+	def get_item_size(self) :
+		sizes = [i.size for i in self.item_size]
+		return sizes 
+
+
+	def uom_list(self):
+		uom_li = [i.uom for i in self.item_uom]
+		return uom_li 
+
+
+
+@frappe.whitelist()
+def validate_uom_self(item , uom , *args , **kwargs) :
+
+	"""
+	item : string item name 
+	uom : string uom name 
+	
+	"""
+	obj = frappe.get_doc("Item" , item) 
+	if uom in obj.uom_list() :
+		return True 
+	else :
+		 return False
+
+
+@frappe.whitelist()
+def get_item_uom_list(doctype, txt, searchfield, start, page_len, filters) :
+	reponse = []
+	if not filters :
+		frappe.throw("filters Missing")
+	print(filters.get("item"))
+	if not filters.get("item") :
+		frappe.throw("Item required ")
+
+	item =  frappe.get_doc("Item" , filters.get("item") ) 
+	data = item.uom_list()
+
+	reponse = ((i,) for i in data)
+	accepted_data = frappe.db.sql(f""" SELECT uom FROM `tabItem UOM` WHERE parent = '{filters.get("item")}' """)
+	#reponse.append(data)
+	print(accepted_data)
+	return reponse
+	# return []
+	
+
---
+++ laziz/laziz/doctype/item_price/item_price.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2024, beshoy atef and contributors
+# For license information, please see license.txt
+
+import frappe
+
+from frappe import throw , _ 
+from frappe.model.document import Document
+
+class ItemPrice(Document):
+	def validate(self) :
+		self.validate_uom()
+
+
+	def validate_uom(self) :
+		if self.uom not in frappe.get_doc("Item" , self.item).uom_list() :
+			throw("Not allowed")
